9f9f6a524fa6cd478ea215444216dc5f87460ba9
LuanRodrigo <luan687@hotmail.com>
2016-11-14 09:55:36 -0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A,B;
+
+    cout << " Digite A e B:";
+    cin >> A >> B;
+
+    cout << " Adicao = " << A + B;
+    cout << "\n Subtracao = " << A - B;
+    cout << "\n Multiplicacao = " << A*B;
+    cout << "\n Divisao = " << A/B;
+    return 0;
+}
